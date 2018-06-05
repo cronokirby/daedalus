4c03f3340225aed8d38eb43f457bd6d71c896603
Lúcás Meier <cronokirby@gmail.com>
2018-06-05 14:53:37 +0200

    adds a basic buffer for scaled pixel drawing

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,61 @@ extern crate minifb;
 use minifb::{Key, WindowOptions, Window};
 
 
-const WIDTH: usize = 800;
-const HEIGHT: usize = 600;
+/// Used to modify pixels but have them printed larger
+struct Buffer {
+    buffer: Vec<u32>,
+    width: usize,
+    height: usize
+}
+
+impl Buffer {
+    /// Constructs a new buffer.
+    /// Width and Height are the number of faux pixels,
+    /// and not actual pixels
+    fn new(width: usize, height: usize) -> Self {
+        Buffer { buffer: vec![0; width * height * 4],
+                 width: width,
+                 height: height }
+    }
+
+    /// Return the bounds of the buffer, in faux pixels.
+    fn bounds(&self) -> (usize, usize) {
+        (self.width, self.height)
+    }
+
+    fn get(&self, row: usize, column: usize) -> u32 {
+        self.buffer[row * self.width * 4 + column * 2]
+    }
+
+    fn set(&mut self, row: usize, column: usize, color: u32) {
+       let mut this_row = row * self.width * 4 + column * 2;
+       for _ in 0..2 {
+            for i in 0..2 {
+                self.buffer[this_row + i] = color;
+            }
+            this_row += self.width * 2;
+       }
+    }
+
+    /// Draws the buffer to a window
+    fn update_window(&self, window: &mut minifb::Window) {
+        window.update_with_buffer(&self.buffer).expect("Failed to draw.");
+    }
+}
+
 
 fn main() {
-    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
+    const WIDTH: usize = 416;
+    const HEIGHT: usize = 320;
+    let mut buffer: Buffer = Buffer::new(WIDTH, HEIGHT);
     let mut window = Window::new(
         "Test - ESC to exit",
-        WIDTH,
-        HEIGHT,
+        WIDTH * 2,
+        HEIGHT * 2,
         WindowOptions::default())
         .expect("Failed to make window");
-
     while window.is_open() && !window.is_key_down(Key::Escape) {
-        for x in buffer.iter_mut() {
-            *x = 0;
-        }
-        window.update_with_buffer(&buffer).unwrap();
+        buffer.update_window(&mut window);
     }
     println!("foo");
 }
